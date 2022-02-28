dd96ad0f973f2e4d94c6d12163a1018fc9d47d66
jihuayu <jihuayu123@outlook.com>
2022-02-28 17:34:37 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cassert>
+#include "leveldb/db.h"
+#include "httplib.h"
+int main() {
+    leveldb::DB* db;
+    leveldb::Options options;
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options, "testdb", &db);
+    assert(status.ok());
+    httplib::Server svr;
+
+    svr.Get(R"(/.+)", [&db](const httplib::Request &req, httplib::Response &res) {
+        auto path = req.path;
+        if (!path.empty()){
+            std::string value;
+            leveldb::Status s = db->Get(leveldb::ReadOptions(), path, &value);
+            if (s.ok()){
+                res.set_redirect(value);
+            }
+            else{
+                res.status = 404;
+            }
+        }
+        else{
+            res.status = 500;
+        }
+    });
+    svr.Put(R"(/.+)", [&db](const httplib::Request &req, httplib::Response &res) {
+        auto path = req.path;
+        auto body = req.body;
+        if (!path.empty()){
+            leveldb::Status s = db->Put(leveldb::WriteOptions(), path, body);
+            if (s.ok()){
+                res.status = 200;
+            }
+            else{
+                res.status = 500;
+            }
+        }
+        else{
+            res.status = 500;
+        }
+    });
+    svr.Delete(R"(/.+)", [&db](const httplib::Request &req, httplib::Response &res) {
+        auto path = req.path;
+        if (!path.empty()){
+            leveldb::Status s = db->Delete(leveldb::WriteOptions(), path);
+            if (s.ok()){
+                res.status = 200;
+            }
+            else{
+                res.status = 500;
+            }
+        }
+        else{
+            res.status = 500;
+        }
+    });
+    svr.listen("0.0.0.0", 8080);
+    delete db;
+    return 0;
+}
